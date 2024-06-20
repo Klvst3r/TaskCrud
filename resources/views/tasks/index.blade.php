1b934b3e7dfd9cde5f9075688d6a0482b6a6ca21
Klvst3r <klvst3r@gmail.com>
2024-06-19 21:04:22 -0600

    Verificación del listado - Estilos

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -14,43 +14,54 @@
 </head>
 
 <body>
-    <table class="table table-striped text-center" border="1">
-        <a href="{{ route('tasks.create') }}" class="btn btn-success">Agregar Tarea</a>
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Descripción</th>
-                <th scope="col">Tipo de Tarea</th>
-                <th scope="col">Atención a usuario</th>
-            </tr>
 
-        </thead>
-        <tbody>
-            @foreach ($task as $item)
-                <tr>
-                    {{-- Muestra el index --}}
-                    <th scope="row">{{ $loop->index + 1 }}</th>
-                    <td>{{ $item->descrip_task }}</td>
-                    <td>{{ $item->tipo_task }}</td>
-                    <td>{{ $item->personal_task }}</td>
-                    <th>
-                        <div class="container">
-                            <div class="row justify-content-md-center">
-                                {{-- <a href="{{ route('tasks.edit', $item) }}" class="btn btn-success col-3">Editar</a>
+    <div class="container mt-3">
+        <div class="card col-12">
+            <table class="table table-striped text-center">
+                <div class="d-grid mt-2">
+                    <div class="offset-5">
+
+                        <a href="{{ route('tasks.create') }}" class="btn btn-success">Agregar Tarea</a>
+                    </div>
+                </div>
+                <div class="d-grid mt-4">
+                    <thead>
+                        <tr>
+                            <th scope="col">No</th>
+                            <th scope="col">Descripción</th>
+                            <th scope="col">Tipo de Tarea</th>
+                            <th scope="col">Atención a usuario</th>
+                            <th scope="col">Acción</th>
+                        </tr>
+
+                    </thead>
+                </div>
+                <tbody>
+                    @foreach ($task as $item)
+                        <tr>
+                            {{-- Muestra el index --}}
+                            <th scope="row">{{ $loop->index + 1 }}</th>
+                            <td>{{ $item->descrip_task }}</td>
+                            <td>{{ $item->tipo_task }}</td>
+                            <td>{{ $item->personal_task }}</td>
+                            <th>
+                                <div class="container">
+                                    <div class="row justify-content-md-center">
+                                        {{-- <a href="{{ route('tasks.edit', $item) }}" class="btn btn-success col-3">Editar</a>
                                 <form action="{{ route('tasks.destroy', $item) }}" method="POST" class="col-3...">
                                     @csrf
                                     @method('delete')
                                     <button type="submit" class="btn btn-danger w-100">Borrar</button>
                                 </form> --}}
-                            </div>
-                        </div>
-                    </th>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-
-
+                                    </div>
+                                </div>
+                            </th>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
